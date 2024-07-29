3a486424d654361236aee79090dfa184165321f2
AbdulazizRus <coderdim@gmail.com>
2024-07-30 01:27:07 +0500

    chasnges

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,11 @@ import Modal from '@/components/Modal'
 import Slider from '@/components/Slider'
 import { homeWork, ourTeam } from '@/constants'
 import {
+  Calendar,
   Facebook,
   Instagram,
   Link,
+  Mails,
   Search,
   Twitter,
   Youtube,
@@ -364,7 +366,59 @@ export default function Home() {
         </div>
       </div>
 
-      <div className="bg-[]"></div>
+      {/* blog */}
+      <div className="bg-[#222222] py-20">
+        <div className="container flex gap-5 max-lg:flex-col justify-between">
+          <div className="md:w-1/2 max-sm:flex-col  flex">
+            <img className='md:w-[300px] h-[400px] ' src="/b.png" alt="" />
+            <div className="bg-[#191919] flex flex-col min-w-72 justify-between p-10">
+              <p className="text-redd flex items-center gap-1">
+                <Calendar width={15} height={15} /> August 9, 2019{' '}
+                <Mails width={15} height={15} /> 0
+              </p>
+              <h1 className="text-white font-bold text-2xl">
+                Every Single Way You Can Wear Pastel Makeup This Spring
+              </h1>
+              <p className="text-base text-[#b3b3b3]">
+                Never ever think of giving up. Winners never quit and
+              </p>
+              <div className="flex gap-2 items-center">
+                <img src="/a1.jpg" className="rounded-full" alt="" />
+                <div className="text-start">
+                  <h2 className="text-white text-lg font-bold">
+                    Aaron Russell
+                  </h2>
+                  <span className="text-redd">DESIGNER</span>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="md:w-1/2 max-sm:flex-col  flex">
+            <img className='md:w-[300px] h-[400px] ' src="/b2.png" alt="" />
+            <div className="bg-[#191919] flex flex-col min-w-72 justify-between p-10">
+              <p className="text-redd flex items-center gap-1">
+                <Calendar width={15} height={15} /> August 9, 2019{' '}
+                <Mails width={15} height={15} /> 0
+              </p>
+              <h1 className="text-white font-bold text-2xl">
+                Every Single Way You Can Wear Pastel Makeup This Spring
+              </h1>
+              <p className="text-base text-[#b3b3b3]">
+                Never ever think of giving up. Winners never quit and
+              </p>
+              <div className="flex gap-2 items-center">
+                <img src="/a1.jpg" className="rounded-full" alt="" />
+                <div className="text-start">
+                  <h2 className="text-white text-lg font-bold">
+                    Aaron Russell
+                  </h2>
+                  <span className="text-redd">DESIGNER</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
 
       {/* modal */}
 

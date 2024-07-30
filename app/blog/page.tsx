c3070d28661c13fb06e4cf08cba4f1f379d32230
AbdulazizRus <coderdim@gmail.com>
2024-07-30 12:45:33 +0500

    asa

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,16 +1,27 @@
+'use client'
+
 import BlogPlace from '@/components/BlogPlace'
 import LinkPage from '@/components/LinkPage'
 import { Calendar, Mails } from 'lucide-react'
+import Aos from 'aos'
+import 'aos/dist/aos.css'
+import { useEffect } from 'react'
 
 const BlogPage = () => {
+  useEffect(() => {
+    Aos.init({
+      duration: 1000,
+      once: true,
+    })
+  }, [])
   return (
     <div>
       <LinkPage path="blog" page="Our Blog" />
       <div className="bg-[#222222]">
-        <div className="container py-24">
+        <div data-aos="fade-down" className="container py-24">
           <div className="grid md:grid-cols-2 grid-cols-1 gap-5">
             <BlogPlace srcA="/author-1.jpg" srcB="/blog-1.jpg" />
-            <BlogPlace srcA="/author-2.jpg" srcB="/blog-2.jpg"  />
+            <BlogPlace srcA="/author-2.jpg" srcB="/blog-2.jpg" />
             <div className="flex flex-col justify-between bg-redd  h-full p-20 text-white">
               <p className="text-white flex items-center gap-1">
                 <Calendar width={15} height={15} /> August 9, 2019{' '}
@@ -36,7 +47,7 @@ const BlogPage = () => {
             <BlogPlace srcA="/author-2.jpg" srcB="/blog-4.jpg" />
             <BlogPlace srcA="/author-2.jpg" srcB="/blog-5.jpg" />
           </div>
-          <div className='flex justify-center mt-16'>
+          <div className="flex justify-center mt-16">
             <button className="border outline-none border-redd px-10 bg-transparent text-white py-3">
               Work With US
             </button>
